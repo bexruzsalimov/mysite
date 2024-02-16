7be642bd303eeb57b56ef6e4f08b7b146e0a55e3
bexruzsalimov <160118018+bexruzsalimov@users.noreply.github.com>
2024-02-16 15:48:56 +0300

    deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,20 +1,24 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+import logging
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv( )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!gtbv1go!y$koz!ekvvk)j=&9nmh_yk8329mso-95)_e$vjcy5'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','' ).split(',')
+
+WEBSITE_URL = os.environ.get('WEBSITE_URL')
 
 
 # Application definition
@@ -32,6 +36,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -41,6 +46,24 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'mysite.urls'
+
+CSRF_TRUSTED_ORIGINS = [
+    f"https:{WEBSITE_URL}",
+    f"https:{WEBSITE_URL}",
+]
+CSRF_COOKIE_DOMAIN = WEBSITE_URL
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_HSTS_SECOND = 3680
+SECURE_HSTS_SECOND_SUBDOMAINS = True # site.mysite.con
+SECURE_HSTS_PRELOAD = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+
+
+
+
+
 
 TEMPLATES = [
     {
@@ -106,6 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STATIC_URL = 'static/'
 
 MEDIA_DIR = BASE_DIR / 'media'
@@ -120,3 +145,24 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'blog:home'
 LOGIN_URL = 'blog:user_logout'
 LOGOUT_REDIRECT_URL = 'blog:home'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+            "propagate": False,
+        },
+    },
+} 
